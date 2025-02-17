213abdc2007620c7330b1a56e4844ff5c6de394e
PurpleDoll <purple.mv@gmail.com>
2025-02-17 18:47:56 -0500

    9. Configure react router dom

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+export default App;
