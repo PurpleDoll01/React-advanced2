fa71c1f6317226d32541305830db5d202e92f4a3
PurpleDoll <purple.mv@gmail.com>
2025-02-17 19:35:56 -0500

    10. Optimización con Rutas  Anidadas y Lazy Loading en React

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { lazy, Suspense } from "react";
+
 import { Routes, Route, Link } from "react-router-dom";
 import Product from "./components/Product";
 import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import Overview from "./components/Overview";
+const Settings = lazy(() => import("./components/Settings"));
 
 function App() {
   return (
@@ -13,6 +18,9 @@ function App() {
           <li>
             <Link to="/about">About</Link>
           </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
         </ul>
       </nav>
 
@@ -20,6 +28,17 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/product/:id" element={<Product />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="overview" element={<Overview />} />
+          <Route
+            path="settings"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Settings />
+              </Suspense>
+            }
+          />
+        </Route>
       </Routes>
     </div>
   );
