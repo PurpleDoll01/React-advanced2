68d4d01cb9e4ee61e86d80188970e5101ac5b1a2
PurpleDoll <purple.mv@gmail.com>
2025-02-17 19:05:20 -0500

    9. Enrutamiento dinámico y navegación programática en React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { Routes, Route, Link } from "react-router-dom";
+import Product from "./components/Product";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/product/:id" element={<Product />} />
       </Routes>
     </div>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
 }
 
 function About() {
